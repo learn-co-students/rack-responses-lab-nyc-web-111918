3315a67d75378eaf9324bf90c5ab84355774ea1e
josh-sea <joshua.cocciardi@gmail.com>
2018-12-09 22:02:42 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+  attr_accessor :now
+
+  def call(env)
+    resp = Rack::Response.new
+    # argument is year, month, day, hour, min
+    if Time.now.hour >= 12
+      ap = "PM"
+      resp.write "Good Afternoon!"
+    else
+      ap = "AM"
+      resp.write "Good Morning!"
+    end
+    resp.write "\n\n#{Time.now.hour}:#{Time.now.min} #{ap}"
+    resp.finish
+  end
+end
--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -1,13 +1,17 @@
 require_relative './spec_helper'
-
+require 'pry'
 describe "App" do
 
   it 'wishes user a good morning in the morning' do
+    # binding.pry
     allow(Time).to receive(:now){
+      # binding.pry
       Time.new(2015,11,27,9,30)
+      # binding.pry
     }
     get '/'
     expect(last_response.body).to include("Morning")
+    # binding.pry
   end
 
   it 'wishes user a good afternoon in the afternoon' do
